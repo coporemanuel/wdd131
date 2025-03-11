2e70486780463e66ca6c9ec094d5b399bd43cf01
coporemanuel <jcoporecutipa@byupathway.edu>
2025-03-11 19:20:17 -0400

    1

---
+++ week02/bookmormon/scripts/script.js
@@ -0,0 +1,10 @@
+const input = doucment.querySelector(`#favchap`);
+const buttom = document.querySelector(`button`);
+const list = document.querySelector(`______`);
+const li = document.createElement(`li`);
+const deletebotton = document.createElement(`button`);
+
+li.textContent = input.value;
+deleteButton.textContent = `❌`;
+li.append(deleteButton);
+list.append(li);
